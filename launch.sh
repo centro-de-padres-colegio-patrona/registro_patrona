7930b1968af3d8d94f51644486eea416c616634e
Leonardo Herrera <l.herreramena@gmail.com>
2026-05-01 23:55:28 -0400

    ngrok service

--- launch.sh
+++ launch.sh
@@ -3,8 +3,10 @@
 set -x
 set -e
 
-ngrok service install --config /ruta/a/tu/ngrok.yml
-ngrok service start
+ROOT_PATH=$(dirname "$(readlink -f "$BASH_SOURCE")")
+
+$ROOT_PATH/ngrok_launch_service.sh
+
 
 #node index.js
 yarn start

b174bc468328ed7757b1e154bb9a130051b495c4
Leonardo Herrera <l.herreramena@gmail.com>
2026-08-10 22:50:36 -0400

    api/evento/estado_de_pago done

--- backend/api_pagos.js
+++ backend/api_pagos.js
@@ -61,21 +61,64 @@ router.get('/evento/estado_de_pago', apiKeyAuth, async (req, res) => {
             for ( let childInfo of user.hijos ) {
                 const estudiante = childInfo['nombre'];
                 //console.log(estudiante);
-                const pagos_estudantes = await db_support.pagosDB.find({id: estudiante});
-                //console.log(`[/api/estado_pago_cpa] pago user: ${JSON.stringify(pago)}`);
+                const pagos_estudantes = await db_support.pagosDB.find({id: estudiante}).lean();
+                //console.log(`[/api/estado_pago_cpa] pagos user: ${JSON.stringify(pagos_estudantes)}`);
                 pagos.push(...pagos_estudantes);
             }
         } else {
             return res.status(404).json({ error: 'Usuario no tiene hijos registrados' });
         }
 
+        //console.log(`${tag} pagos encontrados: `, pagos);
+
+        // Obtener todos los compromisos de pago
+        const compromisos_de_pago_bd = await db_support.compromisosPagoDB.find().lean();
+
         const pagos_estudiantes_splitted_by_compromiso = [];
         for ( const pago of pagos ) {
-            const pagos_splitted = "" // TBD
-            pagos_estudiantes_splitted_by_compromiso.push(pagos_splitted);
+            if (!pago.compromisos_de_pago || pago.compromisos_de_pago.length === 0 || !pago.cantidades || Object.keys(pago.cantidades).length === 0) {
+                const compromiso = compromisos_de_pago_bd.find(c => c.id === pago.tipo);
+                if (compromiso && compromiso.monto > 0) {
+                    const cantidad = pago.monto / compromiso.monto; // Evitar división por cero
+                    pago.cantidad = cantidad;
+                }
+                pagos_estudiantes_splitted_by_compromiso.push(pago);
+                continue;
+            }
+            /*const pagos_splitted = pago.cantidades.map(({ compromiso, cantidad }) => ({
+                ...pago,
+                tipo: compromiso,
+                monto: cantidad * compromisos_de_pago_bd.find(c => c.compromiso === compromiso)?.monto || 0,
+            }));*/
+            // Convertimos el objeto { clave: cantidad } a un arreglo de pares [compromiso, cantidad]
+            const pagos_splitted = Object.entries(pago.cantidades).map(([compromiso, cantidad]) => {
+                const compromisoBD = compromisos_de_pago_bd.find(c => c.id === compromiso);
+                const montoUnitario = compromisoBD ? compromisoBD.monto : 0;
+
+                return {
+                    ...pago,
+                    tipo: compromiso,
+                    monto: cantidad * montoUnitario,
+                    cantidad: cantidad,
+                };
+            });            
+            pagos_estudiantes_splitted_by_compromiso.push(...pagos_splitted);
         }
 
-        //console.log(`${tag} pagos encontrados: `, pagos);
+        //console.log(`${tag} pagos spliteados: `, pagos_estudiantes_splitted_by_compromiso);
+
+        // Reducir pagos por tipo de compromiso.
+        pagos_reducidos_por_compromiso = {};
+        for ( const pago of pagos_estudiantes_splitted_by_compromiso ) {
+            if (!pagos_reducidos_por_compromiso[pago.tipo]) {
+                pagos_reducidos_por_compromiso[pago.tipo] = pago;
+            } else {
+                pagos_reducidos_por_compromiso[pago.tipo].monto += pago.monto;
+                pagos_reducidos_por_compromiso[pago.tipo].cantidad += pago.cantidad;
+            }
+        }
+
+        //console.log(`${tag} pagos reducidos por compromiso: `, pagos_reducidos_por_compromiso);
 
         // Buscar información del evento
         const eventoInfo = await db_support.EventDB.findOne({ id_evento }).lean();
@@ -112,22 +155,31 @@ router.get('/evento/estado_de_pago', apiKeyAuth, async (req, res) => {
             for ( const regla_info of lista_reglas_info ) {
                 //console.log(`${tag} regla_info: `, regla_info);
                 const { compromiso_name, pases_por_compromiso, compromisos_maximo, compromiso_maximo_alcanzado } = regla_info;
-                //console.log(`${tag} compromiso_name: ${compromiso_name}, pases_por_compromiso: ${pases_por_compromiso}, compromisos_maximo: ${compromisos_maximo}, compromiso_maximo_alcanzado: ${compromiso_maximo_alcanzado}`);
+                console.log(`${tag} compromiso_name: ${compromiso_name}, pases_por_compromiso: ${pases_por_compromiso}, compromisos_maximo: ${compromisos_maximo}, compromiso_maximo_alcanzado: ${compromiso_maximo_alcanzado}`);
                 //console.log(`${tag} Filtrando pagos para compromiso_name: ${compromiso_name}, pagos: `, pagos);
-                const pago_filtered = pagos.filter(pago => pago.tipo === compromiso_name || (pago.compromisos_de_pago && pago.compromisos_de_pago.includes(compromiso_name)));
-                console.log(`${tag} pago_filtered for compromiso_name ${compromiso_name}: `, pago_filtered.length);
-                if (pago_filtered.length === 0) continue; // No hay pagos para este compromiso, pasar al siguiente
+                //const pago_filtered = pagos_reducidos_por_compromiso.filter(pago => pago.tipo === compromiso_name || (pago.compromisos_de_pago && pago.compromisos_de_pago.includes(compromiso_name)));
+                if ( !Object.hasOwn(pagos_reducidos_por_compromiso, compromiso_name ) ) continue; 
+                const pago_filtered = pagos_reducidos_por_compromiso[compromiso_name];
+                //console.log(`${tag} pago_filtered for compromiso_name ${compromiso_name}: `, pago_filtered);
+                if (!pago_filtered) continue; // No hay pagos para este compromiso, pasar al siguiente
+
+                // Evaluar Regla
+                const isCompromisoMaximoAlcanzado = pago_filtered.cantidad >= compromisos_maximo;
+                pago_filtered.compromiso_maximo_alcanzado = isCompromisoMaximoAlcanzado;
+                pago_filtered.pases_por_compromiso = pases_por_compromiso;
+                pago_filtered.accion_maximo_alcanzado = compromiso_maximo_alcanzado
+                //console.log(`${tag} pagos_reducidos for compromiso_name ${compromiso_name}: `, pagos_reducidos.length);
                 if (!pagos_por_tipo_entradas[tipo_pase]) {
-                    pagos_por_tipo_entradas[tipo_pase] = [...pago_filtered];
+                    pagos_por_tipo_entradas[tipo_pase] = [pago_filtered];
                 } else {
-                    pagos_por_tipo_entradas[tipo_pase].push(...pago_filtered);
+                    pagos_por_tipo_entradas[tipo_pase].push(pago_filtered);
                 }
             }
         }
 
         const estado_de_pagos = {}
-        for ( const [tipo_pase, lista_pagos] of Object.entries(pagos_por_tipo_entradas) ) {
-            const total_pagos = lista_pagos.length;
+        /*for ( const [tipo_pase, lista_pagos] of Object.entries(pagos_por_tipo_entradas) ) {
+            const total_pagos = lista_pagos.length;*/
             
 
         res.json(pagos_por_tipo_entradas);
